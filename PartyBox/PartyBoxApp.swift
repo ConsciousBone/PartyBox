5656b694e2cedbc2a3981f43f98e407b97727ed0
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-12-03 08:33:17 +0000

    Initial Commit

---
+++ PartyBox/PartyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyBoxApp.swift
+//  PartyBox
+//
+//  Created by Evan Plant on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct PartyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
