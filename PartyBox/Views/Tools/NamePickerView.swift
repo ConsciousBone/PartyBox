2620535e49c1d313a5b0eaf7f06655284e200efe
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-12-05 23:44:55 +0000

    Add adding/removing names to namepickerview

--- PartyBox/Views/Tools/NamePickerView.swift
+++ PartyBox/Views/Tools/NamePickerView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct NamePickerView: View {
+    @State private var names: [String] = []
+    
     var body: some View {
-        Text("Name picker view")
+        Form {
+            Section {
+                ForEach(names.indices, id: \.self) { index in
+                    Text(names[index])
+                }
+                .onDelete(perform: deleteName) // remove the item
+                Button {
+                    withAnimation {
+                        names.append("New name")
+                    }
+                } label: {
+                    Label("Add name", systemImage: "plus")
+                }
+            } header: {
+                Text("Names (\(names.count))")
+            }
+        }
+    }
+    
+    private func deleteName(at offsets: IndexSet) {
+        withAnimation {
+            names.remove(atOffsets: offsets)
+        }
     }
 }
 
