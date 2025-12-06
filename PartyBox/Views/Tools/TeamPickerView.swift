89e5fec6822d88d44ba712a24884cc9f4ca4bb8e
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-12-06 03:49:00 +0000

    Start work on teams in teampickerview

--- PartyBox/Views/Tools/TeamPickerView.swift
+++ PartyBox/Views/Tools/TeamPickerView.swift
@@ -13,17 +13,55 @@ struct Team {
 }
 
 struct TeamPickerView: View {
+    @State private var teamCount = 2
     @State private var teams: [Team] = []
     
     @State private var isEditing = false
     
     var body: some View {
-        VStack {
+        Form {
+            if isEditing {
+                Section {
+                    Stepper(value: $teamCount, in: 1...4) {
+                        Label("\(teamCount) teams", systemImage: "person.3")
+                    }
+                } header: {
+                    Text("Team count")
+                }
+            }
             
+            ForEach(teams.indices, id: \.self) { index in
+                Section {
+                    Text(teams[index].name)
+                    Text("\(teams[index].score)")
+                }
+            }
+            Button("add") {
+                teams.append(Team(name: "New team", score: 0))
+            }
+        }
+        .navigationTitle("Team picker")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button {
+                    withAnimation {
+                        isEditing.toggle()
+                    }
+                } label: {
+                    if isEditing {
+                        Label("Stop editing", systemImage: "pencil.slash")
+                    } else {
+                        Label("Edit", systemImage: "pencil")
+                    }
+                }
+            }
         }
     }
 }
 
 #Preview {
-    TeamPickerView()
+    NavigationStack {
+        TeamPickerView()
+    }
 }
