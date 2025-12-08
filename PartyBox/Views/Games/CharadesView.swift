5e3cc86015415ae3388693079e563af63c0bc4b5
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-12-08 00:26:27 +0000

    Add prompts + random prompt logic

--- PartyBox/Views/Games/CharadesView.swift
+++ PartyBox/Views/Games/CharadesView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct CharadesView: View {
+    @State private var isUsingWords = true
+    @State private var isUsingPhrases = true
+    
+    @State private var selectedPrompt = ""
+    
+    let words = [
+        "Spaceship", "Elephant", "Guitar",
+        "Zombie", "Lighthouse", "Pirate"
+    ]
+    let phrases = [
+        "Walking a dog", "Climbing a ladder", "Washing a car",
+        "Flying a kite", "Riding a bike", "Watering the plants"
+    ]
+    
     var body: some View {
-        Text("Charades")
+        Form {
+            Section {
+                Text(selectedPrompt)
+                    .font(.largeTitle.bold())
+                
+                Button {
+                    getRandomPrompt()
+                } label: {
+                    Label("Shuffle", systemImage: "shuffle")
+                }
+            } header: {
+                Text("Prompt")
+            }
+            .onAppear(perform: getRandomPrompt)
+        }
+    }
+    
+    private func getRandomPrompt() {
+        var promptPool: [String] = []
+        if isUsingWords {
+            promptPool += words
+        }
+        if isUsingPhrases {
+            promptPool += phrases
+        }
+        
+        if promptPool.isEmpty {
+            selectedPrompt = ""
+            return
+        }
+        
+        if let choice = promptPool.randomElement() {
+            selectedPrompt = choice
+        } else {
+            selectedPrompt = "No prompt"
+        }
     }
 }
 
