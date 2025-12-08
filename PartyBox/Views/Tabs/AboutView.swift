d95bfc4089eafe987cfb366ce70ee1e4ee6e06a6
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-12-08 03:49:22 +0000

    Finish aboutview

--- PartyBox/Views/Tabs/AboutView.swift
+++ PartyBox/Views/Tabs/AboutView.swift
@@ -12,6 +12,8 @@ struct AboutView: View {
     let appVersionName = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "unknown"
     let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "unknown"
     
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
         Form {
             Section {
@@ -19,7 +21,50 @@ struct AboutView: View {
                     .font(.largeTitle)
                 Text("Version \(appVersionName) build \(buildNumber)")
             }
+            
+            Section {
+                Text("Developed for Hack Club's Siege Week 13, following a theme of \"Party\".")
+                
+                Button {
+                    print("opening url")
+                    if #available(iOS 26, *) {
+                        openURL(URL(string: "https://hackclub.com/")!, prefersInApp: true)
+                    } else {
+                        openURL(URL(string: "https://hackclub.com/")!)
+                    }
+                } label: {
+                    Label("Learn more about Hack Club", systemImage: "terminal")
+                }
+                
+                Button {
+                    print("opening url")
+                    if #available(iOS 26, *) {
+                        openURL(URL(string: "https://siege.hackclub.com/")!, prefersInApp: true)
+                    } else {
+                        openURL(URL(string: "https://siege.hackclub.com/")!)
+                    }
+                } label: {
+                    Label("Learn more about Siege", systemImage: "crown")
+                }
+            }
+            
+            Section {
+                Text("This app is fully open source! You can see its source code on GitHub.")
+                
+                Button {
+                    print("opening url")
+                    if #available(iOS 26, *) {
+                        openURL(URL(string: "https://github.com/ConsciousBone/PartyBox/")!, prefersInApp: true)
+                    } else {
+                        openURL(URL(string: "https://github.com/ConsciousBone/PartyBox/")!)
+                    }
+                } label: {
+                    Label("Open GitHub repository", systemImage: "terminal")
+                }
+            }
         }
+        .navigationTitle("About")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
