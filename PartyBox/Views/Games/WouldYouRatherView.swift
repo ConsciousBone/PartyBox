8bbbb3d2d3b47d962d94c99726bed5a1c93e7f0c
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-12-08 02:37:46 +0000

    Finish wouldyouratherview
    I really need to remember to commit..

--- PartyBox/Views/Games/WouldYouRatherView.swift
+++ PartyBox/Views/Games/WouldYouRatherView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct WouldYouRatherView: View {
+    @State private var option1 = ""
+    @State private var option2 = ""
+    
+    let options = [
+        "Be able to fly", "Be invisible", "Breathe underwater",
+        "Run at super speed", "Talk to animals", "Speak every language",
+        "Live in space", "Live under the sea", "Never feel cold",
+        "Never feel hot", "Eat anything without getting full",
+        "Never need sleep", "Have perfect memory",
+        "Have unlimited free travel", "Have unlimited free food",
+        "Always be early", "Always be lucky", "Read minds",
+        "Time travel once", "Pause time whenever you want",
+        "Fight one horse-sized duck", "Fight one hundred duck-sized horses",
+        "Lose your phone", "Lose your wallet",
+        "Never listen to music again", "Never watch films again",
+        "Meet your future self", "Meet your past self",
+        "Live in a tiny house", "Live in a massive mansion",
+        "Have perfect vision", "Have perfect hearing",
+        "Only whisper forever", "Only shout forever",
+        "Have a pet dragon", "Have a pet unicorn",
+        "Be the funniest person in the room", "Be the smartest person in the room",
+        "Live without the internet", "Live without your favourite food"
+    ]
+    
     var body: some View {
-        Text("would you rather")
+        Form {
+            Section {
+                Text("Would you rather...")
+                Text(option1)
+                    .font(.headline)
+                Text("or")
+                Text(option2)
+                    .font(.headline)
+            }
+            .onAppear(perform: shuffleOptions)
+            .listRowSeparator(.hidden)
+            
+            Section {
+                Button {
+                    withAnimation {
+                        shuffleOptions()
+                    }
+                } label: {
+                    Text(option1)
+                }
+                
+                Button {
+                    withAnimation {
+                        shuffleOptions()
+                    }
+                } label: {
+                    Text(option2)
+                }
+            }
+        }
+        .navigationTitle("Would you rather")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private func shuffleOptions() {
+        option1 = options.randomElement() ?? "no options??"
+        option2 = options.randomElement() ?? "no options??"
     }
 }
 
