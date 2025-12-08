7d3f9a64e0dfec01e5e84880d26c3913debade06
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-12-08 01:10:43 +0000

    Complete wordguessingview

--- PartyBox/Views/Games/WordGuessingView.swift
+++ PartyBox/Views/Games/WordGuessingView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct WordGuessingView: View {
+    @State private var selectedWord = ""
+    
+    let words = [
+        "Astronaut", "Kangaroo", "Pizza",
+        "Superhero", "Pirate", "Dinosaur",
+        "Robot", "Mermaid", "Detective",
+        "Wizard", "Firefighter", "Vampire",
+        "Princess", "Ninja", "Alien",
+        "Magician", "Chef", "Dragon"
+    ]
+    
     var body: some View {
-        Text("Word guessing")
+        Form {
+            Section {
+                Text(selectedWord)
+                    .font(.largeTitle.bold())
+                
+                Button {
+                    withAnimation {
+                        shuffleWord()
+                    }
+                } label: {
+                    Label("Shuffle", systemImage: "shuffle")
+                }
+            }
+            .onAppear(perform: shuffleWord)
+        }
+        .navigationTitle("Word guessing")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private func shuffleWord() {
+        selectedWord = words.randomElement() ?? "no words??"
     }
 }
 
